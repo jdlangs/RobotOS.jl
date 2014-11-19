1e7e1ddc05e5262fd53f9f24f3ad1f4dbdc21041
Josh Langsfeld <jdlangs@gmail.com>
2014-11-19 12:34:21 -0500

    some cleanup

--- src/ROS.jl
+++ src/ROS.jl
@@ -3,40 +3,15 @@ module ROS
 export genmsgs
 
 using PyCall
-@pyimport rospy
+try
+    @pyimport rospy
+catch ex
+    if ex.val[:args][1] == "No module named rospy"
+        error("rospy not found!\nHas an environment setup script been run?")
+    end
+end
 
 include("time.jl")
-include("ros_types.jl")
-
-function init_node(name::String)
-    rospy.init_node(name)
-end
-
-function subscribe(topic::String, msgtype::String, callback::Function)
-    class = message_class(msgtype)
-    sub = rospy.Subscriber(topic, class, callback)
-    Subscriber(sub)
-end
-
-function advertise(topic::String, msgtype::String)
-    class = message_class(msgtype)
-    pub = rospy.Publisher(topic, class)
-    Publisher(pub)
-end
-
-function spin(t::Real)
-    rospy.sleep(t)
-end
-
-function message_class(msgtype::String)
-    if ! ismatch(r"^\w+/\w+$", msgtype)
-        error("Incorrect message type '$msgtype', use 'package_name/message'")
-    end
-    package, message = split(msgtype, '/')
-    pkg_sym = symbol(package)
-    eval(:(@pyimport $pkg_sym.msg as $pkg_sym))
-    class = eval(:($pkg_sym.pymember($message)))
-    class
-end
+include("gentypes.jl")
 
 end
--- src/gentypes.jl
+++ src/gentypes.jl
@@ -1,11 +1,7 @@
 #Generate Julia composite types for ROS messages
 
-import Base: zero, convert
-
-msg_classes = Dict{String, PyObject}()
-mod_deps = Dict{String, Set{String}}()
-msg_deps = Dict{String, Set{String}}()
-msg_builtin_types = Dict{String, Symbol} (
+_msg_classes = Dict{String, PyObject}()
+_msg_builtin_types = Dict{String, Symbol} (
     "bool"    => :Bool,
     "int8"    => :Int8,
     "int16"   => :Int16,
@@ -18,57 +14,33 @@ msg_builtin_types = Dict{String, Symbol} (
     "float32" => :Float32,
     "float64" => :Float64,
     "string"  => :ASCIIString,
-    "time"    => :Float64,
+    "time"    => :Time,
     )
-zero(::Type{ASCIIString}) = ""
+msgzero{T<:Real}(::Type{T}) = zero(T)
+msgzero(::Type{ASCIIString}) = ""
+msgzero(::Type{Time}) = Time()
 
 function genmsgs(m::Dict{String, Vector{String}})
+    mod_deps = Dict{String, Set{String}}()
+    msg_deps = Dict{String, Set{String}}()
     for (pkg, names) in m
         for n in names
             msgtype = pkg * "/" * n
-            importmsg(msgtype)
+            importmsg(msgtype, mod_deps, msg_deps)
         end
     end
-    modlist = order(mod_deps)
+    modlist = _order(mod_deps)
     for mod in modlist
         modtypes = filter(Regex("^$mod/\\w+\$"), msg_deps)
-        mtypelist = order(modtypes)
-        buildmodule(mod, mtypelist)
+        mtypelist = _order(modtypes)
+        buildmodule(mod, mod_deps[mod], mtypelist)
     end
 end
-
-#Produce an order of the keys of d that respect their dependencies
-function order(d::Dict{String, Set{String}})
-    trecurse!(currlist, d, t) = begin
-        if ! (t in currlist)
-            if haskey(d, t) #do dependencies first
-                for dt in d[t]
-                    trecurse!(currlist, d, dt)
-                end
-                #Now it's ok to add it
-                push!(currlist, t)
-            end
-        end
-    end
-    tlist = ASCIIString[]
-    for t in keys(d)
-        trecurse!(tlist, d, t)
-    end
-    tlist
-end
-
-function pkg_msg_strs(msgtype::String)
-    if ! ismsg(msgtype)
-        error("Incorrect message type '$msgtype', use 'package_name/message'")
-    end
-    split(msgtype, '/')
-end
-ismsg(s::String) = ismatch(r"^\w+/\w+(?:\[\d*\])?$", s)
 
 #Recursively import all needed messages for a given message
-function importmsg(msgtype::String)
-    if ! haskey(msg_classes,msgtype)
-        pkg, msg = pkg_msg_strs(msgtype)
+function importmsg(msgtype::String, mod_deps, msg_deps)
+    if ! haskey(_msg_classes,msgtype)
+        pkg, msg = _pkg_msg_strs(msgtype)
         pkgi = symbol(string("py_",pkg))
         pkgsym = symbol(pkg)
 
@@ -78,45 +50,54 @@ function importmsg(msgtype::String)
             mod_deps[pkg] = Set{String}()
         end
         #Store a reference to the message class definition
-        msg_classes[msgtype] = @eval $pkgi.pymember($msg)
+        _msg_classes[msgtype] = @eval $pkgi.pymember($msg)
         msg_deps[msgtype] = Set{String}()
-        subtypes = msg_classes[msgtype][:_slot_types]
+        subtypes = _msg_classes[msgtype][:_slot_types]
         for t in subtypes
-            if ismsg(t)
-                t, len = check_array_type(t)
-                if pkg != pkg_msg_strs(t)[1]
-                    push!(mod_deps[pkg], pkg_msg_strs(t)[1])
+            if _ismsg(t)
+                t = _check_array_type(t)[1]
+                tpkg = _pkg_msg_strs(t)[1]
+                if tpkg != pkg #Possibly a new module dependency
+                    push!(mod_deps[pkg], tpkg)
                 end
+                #pushing to a set does not create duplicates
                 push!(msg_deps[msgtype], t)
 
-                importmsg(t)
+                importmsg(t, mod_deps, msg_deps)
             end
         end
     end
 end
 
-function buildmodule(modname::String, types::Vector)
+#Create the new module and build the new types inside
+function buildmodule(modname::String, deps::Set, types::Vector)
+    #Create module
     modsym = symbol(modname)
     eval(Expr(:toplevel, :(module ($modsym) end)))
     mod = eval(modsym)
 
-    #Import/exports
+    #Import/exports so the generated code can use external names
     eval(mod, Expr(:using, :PyCall))
+    eval(mod, Expr(:block, 
+        Expr(:using, :., :., :ROS, :Time),
+        Expr(:using, :., :., :ROS, :msgzero),
+    ))
     eval(mod, Expr(:import, :Base, :convert))
-    for m in mod_deps[modname]
+    for m in deps
         eval(mod, Expr(:using, :., :., symbol(m)))
     end
     exports = Expr(:export)
     for typ in types
-        pkg, msg = pkg_msg_strs(typ)
+        pkg, msg = _pkg_msg_strs(typ)
         push!(exports.args, symbol(msg))
     end
     eval(mod, exports)
 
+    #Type creation
     for typ in types
-        pkg, msg = pkg_msg_strs(typ)
-        memnames = msg_classes[typ]["__slots__"]
-        memtypes = msg_classes[typ]["_slot_types"]
+        pkg, msg = _pkg_msg_strs(typ)
+        memnames = _msg_classes[typ]["__slots__"]
+        memtypes = _msg_classes[typ]["_slot_types"]
         members = [zip(memnames, memtypes)...]
         typeexprs = buildtype(msg, members)
 
@@ -124,13 +105,16 @@ function buildmodule(modname::String, types::Vector)
             eval(mod, ex)
         end
     end
+    nothing
 end
 
 #Generate code for a native Julia message type
 #  - type/member declarations
 #  - default constructor
-#  - convert from PyObject
+#  - convert to/from PyObject
 function buildtype(name::String, members::Vector)
+    println("Type: $name")
+
     #Empty expressions
     typedecl = :(
         type $(symbol(name)) 
@@ -149,24 +133,20 @@ function buildtype(name::String, members::Vector)
     consargs = construct.args[2].args
     convargs = pyconvert.args[2].args
 
-    println("Type: $name")
     #Now add the type fields and their implications
     for (n,typ) in members
-        if typ == "time"
-            continue
-        end
-        println("\tmember: $n :: $typ")
+        println("\t$n :: $typ")
 
-        typ, arraylen = check_array_type(typ)
-        if ismsg(typ)
-            j_typ = symbol(pkg_msg_strs(typ)[2])
+        typ, arraylen = _check_array_type(typ)
+        if _ismsg(typ)
+            j_typ = symbol(_pkg_msg_strs(typ)[2])
             j_def = Expr(:call, j_typ)
         else
-            if ! haskey(msg_builtin_types, typ)
+            if ! haskey(_msg_builtin_types, typ)
                 error("Message generation; unknown type '$typ' in:\n$members")
             end
-            j_typ = msg_builtin_types[typ]
-            j_def = Expr(:call, :zero, j_typ)
+            j_typ = _msg_builtin_types[typ]
+            j_def = Expr(:call, :msgzero, j_typ)
         end
 
         namesym = symbol(n)
@@ -186,7 +166,40 @@ function buildtype(name::String, members::Vector)
     [typedecl, construct, pyconvert]
 end 
 
-function check_array_type(typ::String)
+#Produce an order of the keys of d that respect their dependencies
+function _order(d::Dict{String, Set{String}})
+    trecurse!(currlist, d, t) = begin
+        if ! (t in currlist)
+            if haskey(d, t) #do dependencies first
+                for dt in d[t]
+                    trecurse!(currlist, d, dt)
+                end
+                #Now it's ok to add it
+                push!(currlist, t)
+            end
+        end
+    end
+    tlist = String[]
+    for t in keys(d)
+        trecurse!(tlist, d, t)
+    end
+    tlist
+end
+
+function _pkg_msg_strs(msgtype::String)
+    if ! _ismsg(msgtype)
+        error("Incorrect message type '$msgtype', use 'package_name/message'")
+    end
+    split(msgtype, '/')
+end
+#Valid message string is all word chars split by a single forward slash, with
+#optional square brackets for array types
+_ismsg(s::String) = ismatch(r"^\w+/\w+(?:\[\d*\])?$", s)
+
+#Sanitize a string by checking for and removing brackets if they are present
+#Return the sanitized type and the number inside the brackets if it is a fixed
+#size type. Returns -1 if variable size (no number)
+function _check_array_type(typ::String)
     arraylen = -1
     arrtest = r"^([\w/]+)\[(\d*)\]$"
     m = match(arrtest, typ)
