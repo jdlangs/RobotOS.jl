9148ad893a5dc7373686bc4239380c202a523c49
Josh Langsfeld <jdlangs@gmail.com>
2014-11-23 22:48:54 -0500

    generic rospy func definitions

---
+++ src/rospy.jl
@@ -0,0 +1,11 @@
+#General rospy functions
+
+spin()                 = __rospy__.spin()
+is_shutdown()          = __rospy__.is_shutdown()
+get_caller_id()        = __rospy__.get_caller_id()
+get_name()             = __rospy__.get_name()
+get_namespace()        = __rospy__.get_namespace()
+get_node_uri()         = __rospy__.get_node_uri()
+get_param_names()      = __rospy__.get_param_names()
+get_published_topics() = __rospy__.get_published_topics()
+get_ros_root()         = __rospy__.get_ros_root()
