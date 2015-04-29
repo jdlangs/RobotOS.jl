7b5261bc578d1d538251771daaa9c06a9a31ec8a
Josh Langsfeld <jdlangs@gmail.com>
2015-04-29 17:59:43 -0400

    New tests for service type generation

--- test/test_types.jl
+++ test/test_types.jl
@@ -2,10 +2,12 @@
 
 @rosimport std_msgs.msg.Empty
 @rosimport geometry_msgs.msg: PoseStamped, Vector3
+@rosimport nav_msgs.srv.GetPlan
 gentypes()
 
 @test isdefined(:geometry_msgs)
 @test isdefined(:std_msgs)
+@test isdefined(:nav_msgs)
 @test isdefined(geometry_msgs.msg, :Point)
 @test isdefined(geometry_msgs.msg, :Quaternion)
 @test isdefined(geometry_msgs.msg, :Pose)
@@ -13,10 +15,35 @@ gentypes()
 @test isdefined(geometry_msgs.msg, :Vector3)
 @test isdefined(std_msgs.msg, :Header)
 @test isdefined(std_msgs.msg, :Empty)
+@test isdefined(nav_msgs.msg, :Path)
+@test isdefined(nav_msgs.srv, :GetPlan)
+@test isdefined(nav_msgs.srv, :GetPlanRequest)
+@test isdefined(nav_msgs.srv, :GetPlanResponse)
 
 posestamp = geometry_msgs.msg.PoseStamped()
 @test typeof(posestamp.pose) == geometry_msgs.msg.Pose
 @test typeof(posestamp.pose.position) == geometry_msgs.msg.Point
+
+posestamp.pose.position = geometry_msgs.msg.Point(1,2,3)
+pypose = convert(PyObject, posestamp)
+@test pypose[:pose][:position][:x] == 1.
+@test pypose[:pose][:position][:y] == 2.
+@test pypose[:pose][:position][:z] == 3.
+pose2 = convert(geometry_msgs.msg.PoseStamped, pypose)
+@test pose2.pose.position.x == 1.
+@test pose2.pose.position.y == 2.
+@test pose2.pose.position.z == 3.
+
+path = nav_msgs.msg.Path()
+@test typeof(path.poses) == Array{geometry_msgs.msg.PoseStamped,1}
+push!(path.poses, posestamp)
+
+pypath = convert(PyObject, path)
+path2 = convert(nav_msgs.msg.Path, pypath)
+@test typeof(path.poses) == Array{geometry_msgs.msg.PoseStamped,1}
+@test path2.poses[1].pose.position.x == 1.
+@test path2.poses[1].pose.position.y == 2.
+@test path2.poses[1].pose.position.z == 3.
 
 emptymsg = std_msgs.msg.Empty()
 @test length(names(emptymsg)) == 0
