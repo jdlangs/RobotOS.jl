2589d639b4313551815084cfdc8ec048e0b907d1
Josh Langsfeld <jdlangs@gmail.com>
2019-12-20 00:21:46 -0600

    Working message introspection

--- src/ros2/typegen.jl
+++ src/ros2/typegen.jl
@@ -1,46 +1,52 @@
 #typegen.jl
 using Libdl
 
-struct ROSIDLTypeSupport
-    identifer::Cstring
-    data::Ptr{Nothing}
-    func::Ptr{Nothing}
-end
+const ROSIDLMessageTypesupport = rosidl_message_type_support_t
+const ROSIDLMessageMember = rosidl_typesupport_introspection_c__MessageMember
+const ROSIDLMessageMembers = rosidl_typesupport_introspection_c__MessageMembers
 
-struct ROSIDLMessageMember
-    name::Cstring
-    type_id::Cuchar
-    string_upper_bound::Cuint
-    members::Ptr{Nothing}
-    is_array::Bool
-    array_size::Cuint
-    is_upper_bound::Bool
-    offset::Cuint
-    default_value::Ptr{Nothing}
-    size_function::Ptr{Nothing}
-    get_const_function::Ptr{Nothing}
-    get_function::Ptr{Nothing}
-    resize_function::Ptr{Nothing}
-end
-
-struct ROSIDLMessageMembers
-    namespace::Cstring
-    name::Cstring
-    member_count::Cuint
-    size_of::Cuint
-    members::Ptr{ROSIDLMessageMember}
-end
-
+const TYPEID_TO_STRING = Dict(
+    1 => "float",
+    2 => "double",
+    3 => "long double",
+    4 => "char",
+    5 => "wchar",
+    6 => "bool",
+    7 => "octet",
+    8 => "uint8",
+    9 => "int8",
+    10 => "uint16",
+    11 => "int16",
+    12 => "uint32",
+    13 => "int32",
+    14 => "uint64",
+    15 => "int64",
+    16 => "string",
+    17 => "wstring",
+    18 => "ros message",
+)
 
 function get_message_info(pkg, msg)
     func_sym = Symbol("rosidl_typesupport_introspection_c__", "get_message_type_support_handle__", pkg, "__msg__", msg)
     lib = Libdl.dlopen(string("lib", pkg, "__rosidl_typesupport_introspection_c"))
     func_handle = Libdl.dlsym(lib, func_sym)
 
-    typesupport = unsafe_load(ccall(func_handle, Ptr{ROSIDLTypeSupport}, ()))
+    typesupport = unsafe_load(ccall(func_handle, Ptr{ROSIDLMessageTypesupport}, ()))
     return unsafe_load(convert(Ptr{ROSIDLMessageMembers}, typesupport.data))
 end
 
-function print_msg_info(info::ROSIDLMessageMembers)
-    println(unsafe_string(info.namespace), "/", unsafe_string(info.name), ": ", info.member_count, " members")
+function print_msg_info(info::ROSIDLMessageMembers, indent=0)
+    println("  "^indent, unsafe_string(info.message_namespace_), "/", unsafe_string(info.message_name_), ": ", info.member_count_, " members")
+    for i=1:info.member_count_
+        member = unsafe_load(info.members_, i)
+        print_msg_member_info(member, indent+1)
+        if member.type_id_ == 18
+            member_typesupport = unsafe_load(member.members_, 1)
+            print_msg_info(unsafe_load(convert(Ptr{ROSIDLMessageMembers}, member_typesupport.data)), indent+1)
+        end
+    end
+end
+
+function print_msg_member_info(member::ROSIDLMessageMember, indent=0)
+    println("  "^indent, unsafe_string(member.name_), " :: ", TYPEID_TO_STRING[member.type_id_])
 end
