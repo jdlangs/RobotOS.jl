f4f19af847df3db597d00b6993bfe943a6bc1070
HiroIshida <spitfire.docomo@gmail.com>
2020-05-14 23:18:54 +0900

    add test for tf broadcaster

--- test/tf.jl
+++ test/tf.jl
@@ -1,15 +1,9 @@
 #Test if tf listner works correctly
-
-# NOTE delte before push
-using RobotOS # delete
-using Test #delete
 using RobotOS.TF
-
-init_node("jltest", anonymous=true) # delete
-sleep(2)
 
 tl = TransformListener()
 waitForTransform(tl, "parent_link", "child_link", Time(), Duration(1.0))
 transform = lookupTransform(tl, "parent_link", "child_link", Time())
+println("Received Transform: ", transform)
 tf_correct = Transform([0, 0, 0.], [0, 0, 0, 1.])
 @test transform == tf_correct
