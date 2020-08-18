27df02b132157651ba43257f612a9ac38192b3bd
Josh Langsfeld <josh.langsfeld@gmail.com>
2020-08-18 16:49:17 -0500

    Change allowed ROS distros to only currently supported ones

--- test/rospy.jl
+++ test/rospy.jl
@@ -6,7 +6,7 @@ init_node("jltest", anonymous=true)
 #Parameters
 @test length(RobotOS.get_param_names()) > 0
 @test has_param("rosdistro")
-@test chomp(get_param("rosdistro")) in ["hydro", "indigo", "jade", "kinetic", "lunar", "melodic"]
+@test chomp(get_param("rosdistro")) in ["kinetic", "melodic", "noetic"]
 @test ! has_param("some_param")
 @test_throws KeyError get_param("some_param")
 @test_throws KeyError delete_param("some_param")
